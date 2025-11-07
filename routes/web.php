385685ff2bd1571399b3a27ed642c977711f52c0
kat <143475611+puzzlesremixed@users.noreply.github.com>
2025-11-07 18:33:03 +0700

    db update collection settings, multisite

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
 
-Route::get('/api/sites', function () {
+Route::get("/api/sites", function () {
     $sites = collect(Site::all())->map(function ($site) {
         return [
-	    'handle' => $site->handle(),
-            'name' => $site->name(),
-            'locale' => $site->locale(),
-            'url' => $site->url(),
-            'short_locale' => str_replace('_', '-', $site->locale()), // e.g. "en_US" → "en-US"
+            "handle" => $site->handle(),
+            "name" => $site->name(),
+            "locale" => $site->locale(),
+            "url" => $site->url(),
+            "short_locale" => str_replace("_", "-", $site->locale()), // e.g. "en_US" → "en-US"
         ];
     });
 
     return response()->json([
-        'data' => $sites->values(),
-        'count' => $sites->count(),
+        "data" => $sites->values(),
+        "count" => $sites->count(),
     ]);
 });
-
