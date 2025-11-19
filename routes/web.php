41b9121a86a8a21e9cd1c029c5a3c43b0ab2ec32
kat <143475611+puzzlesremixed@users.noreply.github.com>
2025-11-20 04:14:16 +0700

    new sitemap route

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,72 @@
 
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
+use Statamic\Facades\Collection;
 
-Route::get("/api/sites", function () {
-    $sites = collect(Site::all())->map(function ($site) {
-        return [
-            "handle" => $site->handle(),
-            "name" => $site->name(),
-            "locale" => $site->locale(),
-            "url" => $site->url(),
-            "short_locale" => str_replace("_", "-", $site->locale()), // e.g. "en_US" → "en-US"
-        ];
+Route::prefix('api')->group(function () {
+    Route::get("/sites", function () {
+        $sites = collect(Site::all())->map(function ($site) {
+            return [
+                "handle" => $site->handle(),
+                "name" => $site->name(),
+                "locale" => $site->locale(),
+                "url" => $site->url(),
+                "short_locale" => str_replace("_", "-", $site->locale()), // e.g. "en_US" → "en-US"
+            ];
+        });
+
+        return response()->json([
+            "data" => $sites->values(),
+            "count" => $sites->count(),
+        ]);
     });
 
-    return response()->json([
-        "data" => $sites->values(),
-        "count" => $sites->count(),
-    ]);
+    Route::get('/sitemap-data', function () {
+        $collections = Collection::all()->mapWithKeys(function ($collection) {
+            $entries = $collection->queryEntries()->get();
+
+            return [
+                $collection->handle() => $entries->map(function ($entry) {
+                    return [
+                        'id' => $entry->id(),
+                        'locale' => $entry->locale(),
+                        'slug' => $entry->slug(),
+                        'collection' => $entry->collection()->handle(),
+                        'updated_at' => $entry->lastModified(),
+                        'date' => $entry->date(),
+                        'url' => $entry->url(),
+                        'order' => $entry->order(),
+
+                        'parent' => [
+                            'parent_id' => $entry->parent()?->id,
+                            'parent_url' => $entry->parent()?->url
+                        ],
+
+                        'origin' => [
+                            'origin_locale' => $entry->origin()?->locale ?? $entry->locale(),
+                            'origin_id' => $entry->origin()?->id ?? $entry->id(),
+                            'origin_url' => $entry->origin()?->url ?? $entry->url(),
+                        ],
+
+                        'seo' => [
+
+                            'robots' => $entry->robots ? array_map(function ($item) {
+                                return $item['value'];
+                            }, $entry->robots) : null,
+
+                            'sitemap' => [
+                                'priority' => $entry->sitemap_priority,
+                                'include' => $entry->sitemap_include,
+                            ]
+                        ]
+                    ];
+                }),
+            ];
+        });
+
+        return response()->json([
+            'collections' => $collections,
+            "count" => $collections->count(),
+        ]);
+    });
 });
